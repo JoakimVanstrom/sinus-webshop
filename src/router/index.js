7fd2cb69c713efaaf759d877f2e71a6e1b930e9f
SJKed <simonjurstedtskolan@gmail.com>
2022-02-18 11:56:04 +0100

    added single product view to views

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Cart from '../views/Cart.vue'
+import Product from '../views/SingleProductView.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/Cart',
+    name: 'Cart',
+    component: Cart
+
+  },
+  {
+    path: '/Product',
+    name: 'Product',
+    component: Product
+  },
 ]
 
 const router = new VueRouter({
