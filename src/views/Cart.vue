6162d2119828ee0a33c75a61bab98ca52c38379c
99ru <yan.99ru@gmail.com>
2022-02-18 11:42:06 +0100

    added svg icon

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,16 @@
 <template>
   <main>
+
+    <div class="return-arrow">
+      <img src="@/assets/icons/arrow-return-left.svg" height="50px"/>
+    </div>
+
     <h1 class="h1">Mina Piravidsar</h1>
+
+    <section class="products">
+      <div>product 1</div>
+      <div>product 2</div>
+    </section>
 
     <div class="checkout">
       <div class="top">
@@ -8,11 +18,12 @@
         <h2>subtotal</h2>
       </div>
       <div class="bottom">
-          <h2>Total cost</h2>
-          <p>1337 SEK</p>
-          <button>Checkout</button>
+        <h2>Total cost</h2>
+        <p>1337 SEK</p>
+        <button>Checkout</button>
       </div>
     </div>
+
   </main>
 </template>
 
@@ -21,27 +32,41 @@ export default {};
 </script>
 
 <style scoped>
+*{
+  margin: 0;
+}
+.return-arrow{
+ display: grid;
+  place-items: left;
+}
 .h1 {
-  margin-right: 100rem;
+  margin-right: 16rem;
 }
+
 .checkout {
-   display: grid;
-   height: 950px;
-   width: 506px;
-   background-color: gray;
-   place-items: center;
-   margin-left: 85rem;
+ position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  display: grid;
+  height: 950px;
+  width: 506px;
+  background-color: rgba(128, 128, 128, 0.493);
+  place-items: center;
+  margin-left: 85rem;
 }
-button{
-    background-color: #A10000;
-    border-style: none;
-    border-radius: 5px;
-    color: #fff;
-    /* font-size: 1rem; */
-    font-weight: bold;
-    padding: 1rem 2.5rem;
-    text-transform: uppercase;
-    transition: all 0.2s ease-in-out;
-    cursor: pointer;
+.top {
+  margin-bottom: 500px;
+}
+button {
+  background-color: #a10000;
+  border-style: none;
+  border-radius: 5px;
+  color: #fff;
+  /* font-size: 1rem; */
+  font-weight: bold;
+  padding: 1rem 2.5rem;
+  text-transform: uppercase;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
 }
 </style>
