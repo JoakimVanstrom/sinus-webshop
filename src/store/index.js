f7d893c771e6d384df58c661ba7927b65bfbde9a
Psykisk <joakim.vanstrom@gmail.com>
2022-02-21 12:35:12 +0100

    api fetch

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as API from '@/api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    email: null,
   },
   mutations: {
+    saveAuthData(state, authData){
+      state.email = authData.email
+    }
   },
   actions: {
+    async authenticate(context, credentials){
+      const response = await API.login(credentials.email, credentials.password)
+      API.saveToken(response.data.token)
+      context.commit('saveAuthData', response.data)
+    },
+    
   },
   modules: {
   }
